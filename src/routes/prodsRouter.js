b3ec8b456421c5c69a8346e0d675bb6073093375
JuanBenegas <juan.benegas1320@gmail.com>
2023-05-01 21:04:10 -0300

    Se crearon los endpoints get, get-id y post

--- src/routes/prodsRouter.js
+++ src/routes/prodsRouter.js
@@ -1,25 +1,40 @@
 import { Router } from "express";
 import prodsSchema from '../dao/models/prods.model.js'
+import ProductManager from "../dao/productManager.js";
 
-const prodsRouter = Router() 
+const prodsRouter = Router()
+const manager = new ProductManager
 
 prodsRouter.get('/', async (req, res) => {
-    try{
+    try {
         const products = await prodsSchema.find()
-        res.send({status: "Success", products})
+        res.send({ status: "Success", products })
     }
-    catch(e){
+    catch (e) {
         throw new Error("I can't find products.", e)
     }
 })
 
-prodsRouter.post('/', async (req, res) => {
+prodsRouter.get('/:id', async (req, res) => {
     try{
-        const product = await prodsSchema.create(req.body)
-        res.send({status: "Success", product})
+        let {id} = req.params
+        const product = await prodsSchema.findOne({_id: id})
+        res.send({ status: "Success", product })
     }
     catch{
-        res.send({status: "Items incomplete"})
+
+    }
+})
+
+prodsRouter.post('/', async (req, res) => {
+    try {
+        let { name, brand, description, price, stock, image } = req.body
+        if (!name || !brand || !description || !price || !stock || !image) return res.send({ status: "Error", status: "Incomplete items. Use -name, brand, description, price, stock, image-" })
+        const product = await prodsSchema.create(req.body)
+        res.send({ status: "Success", product })
+    }
+    catch {
+        res.send({ status: "Items incomplete" })
     }
 })
 
